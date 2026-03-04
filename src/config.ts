261c245694e9ec9760e3f0fb217d42829822245d
shirakamiNeko <shirakamineko@lolicon.wtf>
2026-03-05 04:40:58 +0800

    chore: Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -213,9 +213,9 @@ export const siteConfig: SiteConfig = {
 		asciiFont: {
 			// 英文字体 - 优先级最高
 			// 指定为英文字体则无论字体包含多大范围，都只会保留 ASCII 字符子集
-			fontFamily: "ZenMaruGothic-Medium",
+			fontFamily: "HYTMR",
 			fontWeight: "400",
-			localFonts: ["ZenMaruGothic-Medium.ttf"],
+			localFonts: ["HYTMR45W.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
 		cjkFont: {
