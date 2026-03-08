5e019c4ee403b12a62a5f89b7114cc24858eb36c
shirakamiNeko <shirakamineko@lolicon.wtf>
2026-03-08 17:41:45 +0800

    refactor: Move kbd css from old blog and rename to keyboard

--- src/plugins/rehype-component-keyboard.js
+++ src/plugins/rehype-component-keyboard.js
@@ -2,9 +2,6 @@
 import { h } from "hastscript";
 
 export function KeyboardComponent(properties, children) {
-	console.log("KeyboardComponent called:", JSON.stringify({ properties, children }, null, 2));
-	
 	const keyText = properties.key || "?";
-
-	return h("kbd", { class: "keyboard" }, keyText);
+	return h("kbd", {}, keyText);
 }
