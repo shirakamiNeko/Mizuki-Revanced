2950b41657325df3e60400ab1c08d2eba95c3b53
星野有希 <shirakamineko@lolicon.io>
2026-04-06 20:55:53 +0800

    refactor: update personal info and banner

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "zh_TW"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "ねこの部屋",
+	title: "",
 	subtitle: "今天喝的是芋頭鮮奶 ~(￣▽￣)~",
 	siteURL: "https://moe.lolicon.io/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2020-09-15", // 站点开始运行日期，用于站点统计组件计算运行天数
@@ -55,11 +55,11 @@ export const siteConfig: SiteConfig = {
 		// 显示模式："text-icon" 显示图标+文本，"logo" 仅显示Logo
 		mode: "text-icon",
 		// 顶栏标题文本
-		text: "MizukiUI",
+		text: "ねこの部屋",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
 		icon: "assets/home/home.webp",
 		// 网站Logo图片路径
-		logo: "assets/home/default-logo.webp",
+		logo: "https://r2.lolicon.io/blog/assets/avatar_512.webp",
 	},
 
 	// 页面自动缩放配置
